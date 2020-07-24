41b0e0a472cc57c686a5b2cde078b0284d1d1153
jafow <jared.a.fowler@gmail.com>
2020-07-24 09:57:02 -0700

    add static

---
+++ precache-manifest.e9c0bb58f38b0eb74936dcf380f95d43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9301ceed27d64368651cb9f6b2c6a32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "829d87dded20e3b077ad",
+    "url": "/static/css/main.5806a8dc.chunk.css"
+  },
+  {
+    "revision": "9fa338aa57ba3b0b4913",
+    "url": "/static/js/2.77177d68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77177d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "829d87dded20e3b077ad",
+    "url": "/static/js/main.96a312b2.chunk.js"
+  },
+  {
+    "revision": "051eecbceed3bd03c537",
+    "url": "/static/js/runtime-main.a3a981a5.js"
+  }
+]);
