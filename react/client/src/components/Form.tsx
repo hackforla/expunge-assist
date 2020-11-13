bc7a525c446a8c23d288586c11b9a535b1e9ce6d
alexchoiweb <alexchoiweb@gmail.com>
2020-11-12 21:54:18 -0800

    build form pages up to page 13

--- react/client/src/components/Form.tsx
+++ react/client/src/components/Form.tsx
@@ -16,13 +16,26 @@ const Form: React.FC<FormProps> = ({ pageNumber, handleClick }) => {
 
   return (
     <Wrapper>
-      <h2>{`<Form>`} state: **for development purposes**</h2>
-      <h2>Name: {name}</h2>
-      <h2>Age: {age}</h2>
-      <h2>Introduction: {introduction} </h2>
       {pageNumber === 1 && <BeforeYouBegin />}
       {pageNumber === 2 && <p>Welcome!</p>}
-      {pageNumber === 3 && <Step1 setName={setName} setAge={setAge} setIntroduction={setIntroduction}/> }
+      {pageNumber === 3 && 
+      <>
+        <Step1 setName={setName} setAge={setAge} setIntroduction={setIntroduction}/>
+        <h2>{`<Form>`} state: **for development purposes**</h2>
+        <h2>Name: {name}</h2>
+        <h2>Age: {age}</h2>
+        <h2>Introduction: {introduction} </h2>
+      </> }
+      {pageNumber === 4 && <p>Congrats!</p> }
+      {pageNumber === 5 && <p>Finish this sentence</p> }
+      {pageNumber === 6 && <p>Way to Go!</p> }
+      {pageNumber === 7 && <p>Please check all that apply:</p> }
+      {pageNumber === 8 && <p>Hooray!</p> }
+      {pageNumber === 9 && <p>Please describe what goals you are working towards acheiving right now. (2 sentences maximum)</p> }
+      {pageNumber === 10 && <p>Great job!</p> }
+      {pageNumber === 11 && <p>Please finish this sentence: I want to clear my record because...</p> }
+      {pageNumber === 12 && <p>Fabulous!</p> }
+      {pageNumber === 13 && <p>Previewing Final Statement</p> }
     </Wrapper>
   )
 }
--- react/client/src/components/Header.tsx
+++ react/client/src/components/Header.tsx
@@ -10,26 +10,39 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ pageNumber }) => {
-  let icon: string, background: string, formStep: number;
+  let icon: string, background: string, formStep: number, showFormHeader: boolean;
   pageNumber === 0 ? icon = iconWhite : icon = iconBlack;
   pageNumber < 1 ? background = '#9903ff' : background = 'white';
-  pageNumber > 2 ? formStep = 1 : formStep = 2;
+  if (pageNumber === 3 ) { formStep=1; showFormHeader=true; }
+  else if (pageNumber === 5) { formStep = 2; showFormHeader=true; }
+  else if (pageNumber === 7) { formStep = 3; showFormHeader=true; }
+  else if (pageNumber === 9) { formStep = 4; showFormHeader=true; }
+  else if (pageNumber === 11) { formStep = 5; showFormHeader=true; }
+  else if (pageNumber === 13) { formStep = 6; showFormHeader=true; }
+  else { formStep = 0; showFormHeader=false }
 
   return (
     <>
-    { pageNumber < 3 ? 
-      <LandingHeader background={background} className="Header">
-        <img src={icon} alt="" />
-        <div>
-          <p>The Record</p>
-          <p>Clearance Project</p>
-        </div>
-      </LandingHeader>
-    : 
+    { showFormHeader ? 
       <FormHeader>
-        <h2>Introduce Yourself!</h2>
-        <p>Step {formStep} of 5</p>
+        {formStep === 1 && <h2>Introduce Yourself!</h2>}
+        {formStep === 2 && <h2>Life Changes</h2>}
+        {formStep === 3 && <h2>Involvement</h2>}
+        {formStep === 4 && <h2>Goals</h2>}
+        {formStep === 5 && <h2>Why</h2>}
+        {formStep === 6 && <h2>My Personal Statement</h2>}
+        
+        {formStep < 6 && <p>Step {formStep} of 5</p> }
+        {formStep === 6 && <p>Completed</p> }
       </FormHeader>
+    : 
+      <LandingHeader background={background} className="Header">
+      <img src={icon} alt="" />
+      <div>
+        <p>The Record</p>
+        <p>Clearance Project</p>
+      </div>
+    </LandingHeader>
     }
     
     </>
