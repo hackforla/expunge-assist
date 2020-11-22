5d21f337463c6ccbff151fb5b27c5f3bea040c75
alexchoiweb <alexchoiweb@gmail.com>
2020-11-21 20:45:49 -0800

    add exact attribute to all routes

--- react/client/src/App.tsx
+++ react/client/src/App.tsx
@@ -14,14 +14,14 @@ const App: React.FC = () => {
   const history = useHistory();
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Switch>
         <Route exact path='/' component={PersonalStatementContainer} history={history}/>
-        <Route path='/form/:page?' component={PersonalStatementContainer} history={history}/>
-        <Route path='/PrivacyPolicy' component={PrivacyPolicy} />
-        <Route path='/TermsOfUse' component={TermsOfUse} />
-        <Route path='/FAQ' component={FAQ} />
-        <Route path='/AboutUs' component={AboutUs} />
+        <Route exact path='/form/:page?' component={PersonalStatementContainer} history={history}/>
+        <Route exact path='/PrivacyPolicy' component={PrivacyPolicy} />
+        <Route exactpath='/TermsOfUse' component={TermsOfUse} />
+        <Route exact path='/FAQ' component={FAQ} />
+        <Route exact path='/AboutUs' component={AboutUs} />
       </Switch>
       <Navbar />
       <GlobalStyle />
