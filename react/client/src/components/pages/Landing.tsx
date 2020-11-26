5c34a67f4a6ad383142d17a05cb6bbd79791e1d6
alexchoiweb <alexchoiweb@gmail.com>
2020-11-25 22:51:20 -0800

    test gh action 01

--- react/client/src/components/pages/Landing.tsx
+++ react/client/src/components/pages/Landing.tsx
@@ -15,7 +15,7 @@ const Landing: React.FC<LandingProps> = ({ goToPage }) => {
       <h2>Generate a personal statement in just 20 minutes</h2>
       <Flex>
         <Button onClick={() => goToPage(1)}>
-          <span>START NOW</span>
+          <span>START NOW 01</span>
           <img src={arrowRight} alt="arrow right" />
         </Button>
       </Flex>
