416fb1c8d54de95b9564a7b9bd5d2b63157eb1bc
alexchoi <alexchoiweb@gmail.com>
2021-01-28 23:03:26 -0800

    first pass implementing material ui

--- react/client/src/App.tsx
+++ react/client/src/App.tsx
@@ -5,6 +5,8 @@ import {
   Route,
   Switch,
 } from 'react-router-dom';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
 
 import PageContainer from 'components/PageContainer';
 import { GlobalStyle } from './styles/GlobalStyle';
@@ -15,26 +17,45 @@ import FAQ from './components/pages/FAQ';
 import AboutUs from './components/pages/AboutUs';
 import Navbar from './components/Navbar';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 320,
+      md: 960,
+      lg: 1200,
+      xl: 1920,
+    },
+  },
+});
+
 const App: React.FC = () => {
   const history = useHistory();
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route exact path="/" component={PageContainer} history={history} />
-        <Route
-          path="/form/:page?"
-          component={PageContainer}
-          history={history}
-        />
-        <Route path="/PrivacyPolicy" component={PrivacyPolicy} />
-        <Route path="/TermsOfUse" component={TermsOfUse} />
-        <Route path="/FAQ" component={FAQ} />
-        <Route path="/AboutUs" component={AboutUs} />
-      </Switch>
-      <Navbar />
-      <GlobalStyle />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route exact path="/" component={PageContainer} history={history} />
+          <Route
+            path="/form/:page?"
+            component={PageContainer}
+            history={history}
+          />
+          <Route path="/PrivacyPolicy" component={PrivacyPolicy} />
+          <Route path="/TermsOfUse" component={TermsOfUse} />
+          <Route path="/FAQ" component={FAQ} />
+          <Route path="/AboutUs" component={AboutUs} />
+        </Switch>
+        <Navbar />
+        <GlobalStyle />
+      </Router>
+    </ThemeProvider>
   );
 };
 
