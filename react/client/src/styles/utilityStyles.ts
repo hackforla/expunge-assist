fb7612f3e4f18a77621ebe203db21d156938fdff
alexchoi <alexchoiweb@gmail.com>
2021-01-30 22:03:54 -0800

    replace one more Button and remove button class from utilityStyles.ts

--- react/client/src/styles/utilityStyles.ts
+++ react/client/src/styles/utilityStyles.ts
@@ -1,11 +1,6 @@
-import { Theme, makeStyles, createStyles } from '@material-ui/core';
+import { makeStyles, createStyles } from '@material-ui/core';
 
-interface StyleProps {
-  theme?: string;
-  hasArrow?: boolean; // todo
-}
-
-const useUtilityStyles = makeStyles<Theme, StyleProps>((theme) =>
+const useUtilityStyles = makeStyles((theme) =>
   createStyles({
     // I think I named this, but I'd like to rename this better because it's not simply a flexbox container, if anyone has ideas - Alex
     flex: {
@@ -26,29 +21,6 @@ const useUtilityStyles = makeStyles<Theme, StyleProps>((theme) =>
       [theme.breakpoints.down('xs')]: {
         marginLeft: 'initial',
         marginRight: 'initial',
-      },
-    },
-    button: {
-      color: '#FFFFFF',
-      padding: '12px 16px',
-      boxShadow: '4px 4px 16px rgba(61, 0, 102, 0.25)',
-      display: 'flex',
-      border: 'none',
-      borderRadius: '24px',
-      fontFamily: 'Roboto',
-      fontStyle: 'normal',
-      fontWeight: 'normal',
-      fontSize: '14px',
-      lineHeight: '16px',
-      letterSpacing: '0.0125em',
-      textTrasnform: 'uppercase',
-      cursor: 'pointer',
-      '& :nth-child(1)': {
-        marginRight: '10px',
-      },
-      background: (props) => (props.theme === 'dark' ? '#25003F' : '#9903FF'),
-      '&:hover': {
-        background: (props) => (props.theme === 'dark' ? '#330652' : '#a224f7'),
       },
     },
   })
