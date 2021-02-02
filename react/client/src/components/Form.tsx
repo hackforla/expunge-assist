1456ff4447f6730485d33ff408be4cb465686c4c
Sarah Park <sarahyaepark@gmail.com>
2021-02-01 19:54:44 -0800

    Feature: Pop up component

--- react/client/src/components/Form.tsx
+++ react/client/src/components/Form.tsx
@@ -11,6 +11,7 @@ import Step3 from './formPages/Step3';
 import Step4 from './formPages/Step4';
 import Step5 from './formPages/Step5';
 import Download from './formPages/Download';
+import PopUp from './PopUp';
 
 interface FormProps {
   pageNumber: number;
@@ -124,6 +125,18 @@ const Form = ({ pageNumber, goToPage, onChangeAffirmation }: FormProps) => {
       {pageNumber === 9 && (
         <Download inputs={inputs} setInputs={setInputs} goToPage={goToPage} />
       )}
+      <PopUp
+        title="Some advice for your personal statement"
+        info={
+          '1. Remember that you are writing for a judge, so refrain from using informal language such as abbreviations or slang' +
+          '\n' +
+          '2. Write in complete sentences when given the option' +
+          '\n' +
+          '3. Use the first person when answering questions. This means telling the story from your point of view.' +
+          '\n' +
+          '4. Please try to limit your responses. We recommend each paragraph being 3-5 sentences.'
+        }
+      />
     </ContentContainer>
   );
 };
