9354901e2f434b583dcaa8329625716736e7c343
qooqu <qooqu@users.noreply.github.com>
2021-07-30 16:25:41 -0400

    added title to new progress bars

--- react/client/src/components/FormHeader.tsx
+++ react/client/src/components/FormHeader.tsx
@@ -74,19 +74,35 @@ function convertStepToTitle(step: string): string {
       return 'Involvement';
     case STEP_ENUMS.INVOLVEMENT.JOB:
       return 'Involvement: Job';
+    case STEP_ENUMS.INVOLVEMENT.JOB_PREVIEW:
+      return 'Involvement: Job';
     case STEP_ENUMS.INVOLVEMENT.COMMUNITY_SERVICE:
+      return 'Involvement: Community Service';
+    case STEP_ENUMS.INVOLVEMENT.COMMUNITY_SERVICE_PREVIEW:
       return 'Involvement: Community Service';
     case STEP_ENUMS.INVOLVEMENT.RECOVERY:
       return 'Involvement: Recovery';
+    case STEP_ENUMS.INVOLVEMENT.RECOVERY_PREVIEW:
+      return 'Involvement: Recovery';
     case STEP_ENUMS.INVOLVEMENT.SCHOOL:
+      return 'Involvement: School';
+    case STEP_ENUMS.INVOLVEMENT.SCHOOL_PREVIEW:
       return 'Involvement: School';
     case STEP_ENUMS.INVOLVEMENT.PARENTING:
       return 'Involvement: Parenting';
+    case STEP_ENUMS.INVOLVEMENT.PARENTING_PREVIEW:
+      return 'Involvement: Parenting';
     case STEP_ENUMS.INVOLVEMENT.UNEMPLOYED:
+      return 'Involvement: Unemployment';
+    case STEP_ENUMS.INVOLVEMENT.UNEMPLOYED_PREVIEW:
       return 'Involvement: Unemployment';
     case STEP_ENUMS.GOALS:
       return 'Goals';
+    case STEP_ENUMS.GOALS_PREVIEW:
+      return 'Goals';
     case STEP_ENUMS.WHY:
+      return 'Why';
+    case STEP_ENUMS.WHY_PREVIEW:
       return 'Why';
     case STEP_ENUMS.FINALIZE:
       return 'My Personal Statement';
