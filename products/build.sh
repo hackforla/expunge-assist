d0f756dbe28305db39e6d7ab8a994967f4360604
aahvocado <dan.dan.makes.stuff@gmail.com>
2021-08-26 18:44:41 -0700

    finish making the build.sh script put things in unified folder

--- products/build.sh
+++ products/build.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 echo "Starting build for expunge-assist project..."
 
-# get latest and install
-git checkout . && git clean -df && git pull && npm install
+MAIN_DIR=$PWD
+LANDING_DIR="$MAIN_DIR/landing-page"
+STATEMENT_DIR="$MAIN_DIR/statement-generator"
 
-# build the multiple parts of the app
-git checkout ./landing-page && npm run build
-git checkout ./statement-generator && npm run build
+# prepare unified folder
+cd $MAIN_DIR && mkdir -p builds
 
-# bring the build to a unified location
-mv ./landing-page/build ../build/landing-bundle
-mv ./statement-generator/build ../build/statement-bundle
+# build each project and move them into the unified folder
+cd $LANDING_DIR && npm i && npm run build
+mv $LANDING_DIR/public/ $MAIN_DIR/builds/landing-bundle/
+
+cd $STATEMENT_DIR && npm i && npm run build
+mv $STATEMENT_DIR/build/ $MAIN_DIR/builds/statement-bundle/
 
 echo "Finished unified expunge-assist build."
