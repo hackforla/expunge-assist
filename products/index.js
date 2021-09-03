8e5e5837775cb4d858bfd257f40f2fec628d0192
aahvocado <dan.dan.makes.stuff@gmail.com>
2021-09-02 18:06:35 -0700

    routing for each project

--- products/index.js
+++ products/index.js
@@ -1,15 +1,26 @@
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const express = require('express');
-express.static('/build');
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
+const landingFilepath = path.join(__dirname, "/builds/landing-bundle/");
+const statementFilepath = path.join(__dirname, "/builds/statement-bundle/");
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/builds/landing-bundle/index.html'));
+app.use("/statement-generator/static", express.static(path.join(statementFilepath, "/static/")));
+app.use("/static", express.static(path.join(landingFilepath, "/static/")));
+app.use("/statement-generator/static", express.static(statementFilepath));
+app.use("/static", express.static(landingFilepath));
+
+app.get("/statement-generator*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/builds/statement-bundle/index.html"));
+});
+
+app.get("/*", function (req, res) {
+  const landingIndexFilepath = path.join(landingFilepath, "index.html");
+  res.sendFile(landingIndexFilepath);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`App initialized at http://localhost:${port}`);
+});
