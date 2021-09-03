38d37a391c406be4735fc8df31c93a7b2a45940b
aahvocado <dan.dan.makes.stuff@gmail.com>
2021-09-02 18:05:23 -0700

    finalize build.sh script

--- products/build.sh
+++ products/build.sh
@@ -6,13 +6,17 @@ LANDING_DIR="$MAIN_DIR/landing-page"
 STATEMENT_DIR="$MAIN_DIR/statement-generator"
 
 # prepare unified folder
-cd $MAIN_DIR && mkdir -p builds
+rm -rf $MAIN_DIR/builds
+mkdir -p $MAIN_DIR/builds
 
 # build each project and move them into the unified folder
 cd $LANDING_DIR && npm i && npm run build
 mv $LANDING_DIR/public/ $MAIN_DIR/builds/landing-bundle/
+# mv $MAIN_DIR/builds/landing-bundle/public/static/ $MAIN_DIR/builds/static/
+
 
 cd $STATEMENT_DIR && npm i && npm run build
 mv $STATEMENT_DIR/build/ $MAIN_DIR/builds/statement-bundle/
+# mv $STATEMENT_DIR/builds/statement-bundle/build/static/ $MAIN_DIR/builds/static/
 
 echo "Finished unified expunge-assist build."
