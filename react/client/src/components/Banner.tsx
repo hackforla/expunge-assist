d96a48bcfcd4cc934a85eee1f386b511d7efc89b
aahvocado <dan.dan.makes.stuff@gmail.com>
2021-10-15 19:19:41 -0700

    no height so it adjusts for mobile

--- react/client/src/components/Banner.tsx
+++ react/client/src/components/Banner.tsx
@@ -5,12 +5,11 @@ const useStyles = makeStyles(() =>
   createStyles({
     root: {
       width: '100%',
-      height: '30px',
+      padding: '5px',
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
       fontSize: '15px',
-      borderRadius: '3px',
       marginTop: '10px',
       backgroundColor: '#ff3403',
       color: '#222',
