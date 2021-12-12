d9cde1e84c17ef84426a898560579b30a18a711c
aahvocado <dan.dan.makes.stuff@gmail.com>
2021-12-11 19:16:42 -0800

    create moduleAugmentation.ts for extending palette options

---
+++ products/statement-generator/src/moduleAugmentation.ts
@@ -0,0 +1,10 @@
+import { PaletteColorOptions } from '@material-ui/core/styles/createPalette';
+
+declare module '@material-ui/core/styles/createPalette' {
+  export interface PaletteOptions {
+    tertiary?: PaletteColorOptions;
+  }
+  export interface SimplePaletteColorOptions {
+    darker?: string;
+  }
+}
