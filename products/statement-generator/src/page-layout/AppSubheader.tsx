9baec6fda1cb02b0169c50c5da04867001a715c0
aahvocado <dan.dan.makes.stuff@gmail.com>
2022-01-06 14:10:59 -0800

    move the FormHeader progress bar to AppSubheader

--- products/statement-generator/src/page-layout/AppSubheader.tsx
+++ products/statement-generator/src/page-layout/AppSubheader.tsx
@@ -2,9 +2,10 @@ import React, { useContext } from 'react';
 import { Theme, makeStyles, createStyles } from '@material-ui/core';
 
 import { Banner } from 'components/Banner';
+import FormHeader from 'components/FormHeader';
 
 import RoutingContext from 'contexts/RoutingContext';
-import { STEP_ENUMS } from 'contexts/RoutingProps';
+import { STEP_ENUMS, isFormPage } from 'contexts/RoutingProps';
 
 const useStyles = makeStyles<Theme, IUseUtilityStyle>(({ palette }) =>
   createStyles({
@@ -43,13 +44,17 @@ const useStyles = makeStyles<Theme, IUseUtilityStyle>(({ palette }) =>
 const AppSubheader = () => {
   const { currentStep } = useContext(RoutingContext);
 
-  const classes = useStyles({ pageTheme: 'dark' });
-
   const isLandingPage = currentStep === STEP_ENUMS.NONE;
+
+  const classes = useStyles({
+    pageTheme: isLandingPage ? 'dark' : 'transparent',
+  });
 
   return (
     <div className={classes.subheaderContainer}>
       {isLandingPage && <Banner />}
+
+      {isFormPage(currentStep) && <FormHeader />}
     </div>
   );
 };
