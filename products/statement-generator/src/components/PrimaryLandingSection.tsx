eeb1204626cd790aa8f5ac4fe7ac04c7ec4e8931
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-01-19 16:48:00 -0800

    import utility styles

--- products/statement-generator/src/components/PrimaryLandingSection.tsx
+++ products/statement-generator/src/components/PrimaryLandingSection.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import useUtilityStyles from 'styles/utilityStyles';
 
 const PrimaryLandingSection = () => {
+    const utilityClasses = useUtilityStyles();
+    console.log(utilityClasses)
     return (
         <>
+        hello world
         </>
     );
 };
