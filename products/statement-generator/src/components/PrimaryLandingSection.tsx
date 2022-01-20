91efa103b3e6acbfadaaba5afd9890aa0432fc43
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-01-20 12:42:57 -0800

    added 'tangerine' theme to utility styles to access tangerine color in palette, displaying tangerine color as background of section through utility classes and removed hardcoding

--- products/statement-generator/src/components/PrimaryLandingSection.tsx
+++ products/statement-generator/src/components/PrimaryLandingSection.tsx
@@ -3,23 +3,17 @@ import React from 'react';
 import useUtilityStyles from 'styles/utilityStyles';
 
 const PrimaryLandingSection = () => {
-    const utilityClasses = useUtilityStyles();
-    const css = `
-        #primary-landing-section {
-            background-color: #FFFAF2
-        }
-    `
+    const utilityClasses = useUtilityStyles({
+        pageTheme: 'tangerine'
+    });
 
     return (
-        <>
-            <section id='primary-landing-section'>
-                <div className={utilityClasses.contentContainer}>
-                    <div><span>Expunge Assist</span> accelerates the <span className="recordClearancePurple">Record Clearance</span> process by helping user generate a declaration letter</div>
-                    <div>While still under development, Expunge Assist will aim to help people in California with criminal records accomplish record clearance, expungement or reduction.</div>
-                </div>
-            </section>
-            <style>{css}</style>
-        </>
+        <section className ={utilityClasses.primaryContainer}>
+            <div className={utilityClasses.contentContainer}>
+                <div><span>Expunge Assist</span> accelerates the <span>Record Clearance</span> process by helping user generate a declaration letter</div>
+                <div>While still under development, Expunge Assist will aim to help people in California with criminal records accomplish record clearance, expungement or reduction.</div>
+            </div>
+        </section>
     );
 };
 
--- products/statement-generator/src/styles/utilityStyles.ts
+++ products/statement-generator/src/styles/utilityStyles.ts
@@ -16,6 +16,8 @@ const useUtilityStyles = makeStyles<Theme>(
               return palette.primary.main;
             case 'light':
               return palette.primary.light;
+            case 'tangerine':
+              return palette.secondary.main;
             case 'transparent':
             default:
               return 'transparent';
