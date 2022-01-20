86f85e29cccd0f88edc265f9e7c7bfb2a0de3223
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-01-19 18:32:28 -0800

    hardcoded css styles in to get the background

--- products/statement-generator/src/components/PrimaryLandingSection.tsx
+++ products/statement-generator/src/components/PrimaryLandingSection.tsx
@@ -4,14 +4,21 @@ import useUtilityStyles from 'styles/utilityStyles';
 
 const PrimaryLandingSection = () => {
     const utilityClasses = useUtilityStyles();
+    const css = `
+        #primary-landing-section {
+            background-color: #FFFAF2
+        }
+    `
+
     return (
         <>
-            <section>
+            <section id='primary-landing-section'>
                 <div className={utilityClasses.contentContainer}>
-                    <div>Expunge Assist accelerates the Record Clearance process by helping user generate a declaration letter</div>
+                    <div><span>Expunge Assist</span> accelerates the <span className="recordClearancePurple">Record Clearance</span> process by helping user generate a declaration letter</div>
                     <div>While still under development, Expunge Assist will aim to help people in California with criminal records accomplish record clearance, expungement or reduction.</div>
                 </div>
             </section>
+            <style>{css}</style>
         </>
     );
 };
