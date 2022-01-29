5954412222328ff292d70f3cd8534569fd2b599c
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-01-29 15:55:49 -0800

    add contentContainer component, clean up styles to basics, created landingImage and landingImageAlt variables to make substitution easier

--- products/statement-generator/src/components/PrimaryLandingSection.tsx
+++ products/statement-generator/src/components/PrimaryLandingSection.tsx
@@ -3,6 +3,7 @@ import { Theme, makeStyles, createStyles } from '@material-ui/core';
 
 import useUtilityStyles from 'styles/utilityStyles';
 import Button from 'components/Button';
+import ContentContainer from 'page-layout/ContentContainer';
 
 interface ComponentProps {
     goNextPage: () => void;
@@ -12,18 +13,41 @@ interface StyleProps {
     theme?: string;
 }
 
-const useStyles = makeStyles<Theme, StyleProps>(({ palette, typography, spacing }) =>
+const useStyles = makeStyles<Theme, StyleProps>(({ palette, typography, spacing, breakpoints }) =>
     createStyles({
-        outer: {
+        outerWrapper: {
             background: palette.secondary.main,
-        },
-        content: {
             color: palette.primary.darker,
+            minHeight: '686px',
+        },
+        title: {
             fontFamily: typography.fontFamily,
-            padding: spacing(3),
+            fontSize: '40px',
+            
+            [breakpoints.down('sm')]: {
+                fontSize: '24px',
+                lineHeight: '117%'
+            }
         },
         span: {
-            color: palette.primary.main
+            color: palette.primary.main,
+        },
+        subtitle: {
+            fontFamily: typography.fontFamily,
+            fontSize: '25px',
+            lineHeight: '160%',
+            
+            [breakpoints.down('sm')]: {
+                fontSize: '20px',
+                width: '100%'
+            }
+        },
+        image: {
+            flex: '1 0 auto',
+            maxWidth: '590px',
+            [breakpoints.down('sm')]: {
+                order: 2
+            }
         },
     })
 )
@@ -31,15 +55,14 @@ const useStyles = makeStyles<Theme, StyleProps>(({ palette, typography, spacing 
 const PrimaryLandingSection = ({ goNextPage }: ComponentProps) => {
     const utilityClasses = useUtilityStyles();
     const classes = useStyles({ theme: 'light' });
+    const landingImage = "https://via.placeholder.com/590x350";
+    const landingImageAlt = 'A placeholder waiting for the correct image'
 
     return (
-        <section className ={classes.outer}>
-            <div className={utilityClasses.contentContainer}>
-                <div className={classes.content}>
-                    <h1><span className={classes.span}>Expunge Assist</span> accelerates the <span className={classes.span}>Record Clearance</span> process by helping user generate a declaration letter</h1>
-                    <p className = {classes.p}>While still under development, Expunge Assist will aim to help people in California with criminal records accomplish record clearance, expungement or reduction.</p>
-                </div>
-                <img src="https://via.placeholder.com/590x350" alt="" />
+        <section className ={classes.outerWrapper}>
+            <ContentContainer>
+                <h1 className={classes.title}><span className={classes.span}>Expunge Assist</span> accelerates the <span className={classes.span}>Record Clearance</span> process by helping user generate a declaration letter</h1>
+                <p className = {classes.subtitle}>While still under development, Expunge Assist will aim to help people in California with criminal records accomplish record clearance, expungement or reduction.</p>
                 <div className={utilityClasses.buttonContainer}>
                     <Button
                         theme='landing'
@@ -47,7 +70,8 @@ const PrimaryLandingSection = ({ goNextPage }: ComponentProps) => {
                         onClick={() => goNextPage()}
                     />
                 </div>
-            </div>
+                <img src={landingImage} alt={landingImageAlt} className={classes.image}/>
+            </ContentContainer>
         </section>
     );
 };
