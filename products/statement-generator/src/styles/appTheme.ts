22919f251dd1406a566539e1f6b6f95ead222ed1
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-02 10:34:52 -0800

    remove darker purple from secondary palette

--- products/statement-generator/src/styles/appTheme.ts
+++ products/statement-generator/src/styles/appTheme.ts
@@ -16,7 +16,6 @@ const appTheme = createMuiTheme({
     },
     secondary: {
       main: '#FFFAF2', // tangerine light orange
-      dark: '#7422DA',
     },
     common: {
       black: '#0a0a0a',
