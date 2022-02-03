1b80141f8c9d6179d1fa122b82b819f1dc41fbd5
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-03 09:56:21 -0800

    ran prettier to make it prettier :)

--- products/statement-generator/src/components/PrimaryLandingSection.tsx
+++ products/statement-generator/src/components/PrimaryLandingSection.tsx
@@ -9,109 +9,126 @@ interface ComponentProps {
 }
 
 interface StyleProps {
-    theme?: string;
+  theme?: string;
 }
 
-const useStyles = makeStyles<Theme, StyleProps>(({ palette, typography, spacing, breakpoints }) =>
+const useStyles = makeStyles<Theme, StyleProps>(
+  ({ palette, typography, spacing, breakpoints }) =>
     createStyles({
-        root: {
-            background: palette.secondary.main,
-            color: palette.primary.darker,
-            display: 'flex',
-            flexDirection: 'row',
-            padding: spacing(12, 3),
-            [breakpoints.down('sm')]: {
-                padding: spacing(3)
-            }
+      root: {
+        background: palette.secondary.main,
+        color: palette.primary.darker,
+        display: 'flex',
+        flexDirection: 'row',
+        padding: spacing(12, 3),
+        [breakpoints.down('sm')]: {
+          padding: spacing(3),
         },
-        content: {
-            padding: spacing(2,3),
-            maxWidth: '1300px',
-            display: 'flex',
-            flex: 1,
-            flexFlow: 'row',
-            margin: '0 auto',
-            alignItems: 'center',
+      },
+      content: {
+        padding: spacing(2, 3),
+        maxWidth: '1300px',
+        display: 'flex',
+        flex: 1,
+        flexFlow: 'row',
+        margin: '0 auto',
+        alignItems: 'center',
+      },
+      title: {
+        fontFamily: typography.fontFamily,
+        fontSize: '40px',
+
+        [breakpoints.down('sm')]: {
+          fontSize: '24px',
+          lineHeight: '117%',
         },
-        title: {
-            fontFamily: typography.fontFamily,
-            fontSize: '40px',
-            
-            [breakpoints.down('sm')]: {
-                fontSize: '24px',
-                lineHeight: '117%'
-            }
+      },
+      span: {
+        color: palette.primary.main,
+      },
+      text: {
+        paddingRight: spacing(3),
+        [breakpoints.down('sm')]: {
+          display: 'flex',
+          flexDirection: 'column',
         },
-        span: {
-            color: palette.primary.main,
+      },
+      subtitle: {
+        fontFamily: typography.fontFamily,
+        fontSize: '25px',
+        lineHeight: '160%',
+
+        [breakpoints.down('sm')]: {
+          fontSize: '20px',
+          width: '100%',
         },
-        text: {
-            paddingRight: spacing(3),
-            [breakpoints.down('sm')]: {
-                display: 'flex',
-                flexDirection: 'column'
-            }
+      },
+      button: {
+        paddingTop: spacing(3),
+        [breakpoints.down('sm')]: {
+          margin: '0 auto',
         },
-        subtitle: {
-            fontFamily: typography.fontFamily,
-            fontSize: '25px',
-            lineHeight: '160%',
-            
-            [breakpoints.down('sm')]: {
-                fontSize: '20px',
-                width: '100%'
-            }
+      },
+      mobileImg: {
+        display: 'none',
+        [breakpoints.down('sm')]: {
+          display: 'flex',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          marginTop: spacing(2),
+          marginBottom: spacing(2),
+          width: '100%',
         },
-        button: {
-            paddingTop: spacing(3),
-            [breakpoints.down('sm')]: {
-                margin: '0 auto'
-            }
+      },
+      desktopImg: {
+        width: '50%',
+        [breakpoints.down('sm')]: {
+          display: 'none',
         },
-        mobileImg: {
-            display: 'none',
-            [breakpoints.down('sm')]: {
-                display: 'flex',
-                marginLeft: 'auto',
-                marginRight: 'auto',
-                marginTop: spacing(2),
-                marginBottom: spacing(2),
-                width: '100%'
-            }
-        },
-        desktopImg :{
-            width: '50%',
-            [breakpoints.down('sm')]: {
-                display: 'none'
-            }
-        }
+      },
     })
-)
+);
 
 const PrimaryLandingSection = ({ goNextPage }: ComponentProps) => {
-    const classes = useStyles({ theme: 'light' });
-    const landingImage = "https://via.placeholder.com/590x350";
-    const landingImageAlt = 'A placeholder waiting for the correct image';
+  const classes = useStyles({ theme: 'light' });
+  const landingImage = 'https://via.placeholder.com/590x350';
+  const landingImageAlt = 'A placeholder waiting for the correct image';
 
-    return (
-        <section className ={classes.root}>
-            <ContentContainer className =  {classes.content}>
-                <div className = {classes.text}>
-                    <h1 className={classes.title}><span className={classes.span}>Expunge Assist</span> accelerates the <span className={classes.span}>Record Clearance</span> process by helping user generate a declaration letter</h1>
-                    <img src={landingImage} alt={landingImageAlt} className={classes.mobileImg}/>
-                    <p className = {classes.subtitle}>While still under development, Expunge Assist will aim to help people in California with criminal records accomplish record clearance, expungement or reduction.</p>
-                    <div className={classes.button}>
-                        <Button
-                            theme='landing'
-                            buttonText='View Demo'
-                            onClick={() => goNextPage()}
-                        />
-                    </div>
-                </div>
-                <img src={landingImage} alt={landingImageAlt} className={classes.desktopImg}/>
-            </ContentContainer>
-        </section>
-    );
+  return (
+    <section className={classes.root}>
+      <ContentContainer className={classes.content}>
+        <div className={classes.text}>
+          <h1 className={classes.title}>
+            <span className={classes.span}>Expunge Assist</span> accelerates the{' '}
+            <span className={classes.span}>Record Clearance</span> process by
+            helping user generate a declaration letter
+          </h1>
+          <img
+            src={landingImage}
+            alt={landingImageAlt}
+            className={classes.mobileImg}
+          />
+          <p className={classes.subtitle}>
+            While still under development, Expunge Assist will aim to help
+            people in California with criminal records accomplish record
+            clearance, expungement or reduction.
+          </p>
+          <div className={classes.button}>
+            <Button
+              theme="landing"
+              buttonText="View Demo"
+              onClick={() => goNextPage()}
+            />
+          </div>
+        </div>
+        <img
+          src={landingImage}
+          alt={landingImageAlt}
+          className={classes.desktopImg}
+        />
+      </ContentContainer>
+    </section>
+  );
 };
 
 export default PrimaryLandingSection;
