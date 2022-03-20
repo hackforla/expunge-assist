687852a8dc5574c02a89079f741ef3faffdb5904
aahvocado <dan.dan.makes.stuff@gmail.com>
2022-03-20 14:18:38 -0700

    remove Welcome text from Affirmation

--- products/statement-generator/src/pages-form/Welcome.tsx
+++ products/statement-generator/src/pages-form/Welcome.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import ContentContainer from 'components-layout/ContentContainer';
 import FlowNavigation from 'components-layout/FlowNavigation';
 
 export default function Welcome() {
+  const { t } = useTranslation();
+
   return (
     <ContentContainer>
-      <h1>Welcome</h1>
+      <h1>{t('welcomePage.titleText')}</h1>
 
-      <p>
-        This tool will guide you through writing your declaration letter. You
-        can preview your letter and make changes at the end of each section.
-      </p>
+      <p>{t('welcomePage.description')}</p>
 
       <FlowNavigation showBack={false} />
     </ContentContainer>
