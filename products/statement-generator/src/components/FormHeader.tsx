396e7cae8f18f2cf7eeb375b6aee6ce1ec4f035d
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-04-01 17:10:58 -0700

    it is so ugly, but it works and its mildly dynamic lol

--- products/statement-generator/src/components/FormHeader.tsx
+++ products/statement-generator/src/components/FormHeader.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Theme, makeStyles, createStyles } from '@material-ui/core';
 
 import RoutingContext from 'contexts/RoutingContext';
 import { AppUrl } from 'contexts/RoutingProps';
 
 import ProgressBar from 'components/ProgressBar';
+import FormStateContext from 'contexts/FormStateContext';
+import { IInvolvementInitialState } from 'contexts/FormStateProps';
 
 const useStyles = makeStyles<Theme>(
   ({ palette, breakpoints, spacing, typography }) =>
@@ -32,35 +34,66 @@ const useStyles = makeStyles<Theme>(
     })
 );
 
-function convertStepToNum(url: AppUrl): number {
+function convertStepToNum(url: AppUrl, involvement: IInvolvementInitialState ): number {
+  let additionalStepCounter =0;
+  // get the boolean values of involvement
+  const involvementArr = Object.values(involvement);
+  // isJobChecked and isNoneChecked are already accounted for; so we're removing them
+  involvementArr.shift();
+  involvementArr.pop();
+  const involvementActivities: number[] = [];
+  involvementArr.forEach((value,i) => {
+    if(value) {
+      additionalStepCounter += 1;
+      involvementActivities.push(0);
+      return;
+    }
+    involvementActivities.push(-1)
+  })
+  const stepAdjustment: number[] = [];
+  involvementActivities.reduce((a,b) => {
+    stepAdjustment.push(b);
+    return a+b;
+  })
+  console.log(stepAdjustment);
+  
   switch (url) {
     case AppUrl.Introduction:
     case AppUrl.IntroductionPreview:
       return 1;
     case AppUrl.Involvement:
+      return 2;
+    case AppUrl.CommunityService:
+    case AppUrl.CommunityServicePreview: {
+      return 7 + stepAdjustment[3];
+    }
+    case AppUrl.Recovery:
+    case AppUrl.RecoveryPreview: {
+      return 4;
+    }
+    case AppUrl.School:
+    case AppUrl.SchoolPreview: {
+      return 5 + stepAdjustment[1];
+    }
+    case AppUrl.Parenting:
+    case AppUrl.ParentingPreview: {
+      return 6 + stepAdjustment[2];
+    }
     case AppUrl.Job:
     case AppUrl.JobPreview:
-    case AppUrl.CommunityService:
-    case AppUrl.CommunityServicePreview:
-    case AppUrl.Recovery:
-    case AppUrl.RecoveryPreview:
-    case AppUrl.School:
-    case AppUrl.SchoolPreview:
-    case AppUrl.Parenting:
-    case AppUrl.ParentingPreview:
     case AppUrl.Unemployed:
     case AppUrl.UnemployedPreview:
-      return 2;
+      return 3;
     case AppUrl.Goals:
     case AppUrl.GoalsPreview:
-      return 3;
+      return 4 + additionalStepCounter;
     case AppUrl.Why:
     case AppUrl.WhyPreview:
-      return 4;
+      return 5 + additionalStepCounter;
     case AppUrl.Finalize:
-      return 5;
+      return 6 + additionalStepCounter;
     case AppUrl.Download:
-      return 5;
+      return 6 + additionalStepCounter;
     default:
       return 0;
   }
@@ -114,10 +147,30 @@ function convertStepToTitle(url: AppUrl): string {
 
 const FormHeader = () => {
   const classes = useStyles();
-  const { currentStep } = React.useContext(RoutingContext);
+  const { currentStep } = useContext(RoutingContext);
+  const { formState: { involvement } } = useContext(FormStateContext)
 
-  const maxNum = 5;
-  const stepNum = convertStepToNum(currentStep);
+  const { isJobChecked, isCommunityChecked, isParentingChecked, isSchoolChecked, isRecoveryChecked, isNoneChecked } = involvement;
+  let maxNum = 5;
+  const stepNum = convertStepToNum(currentStep, involvement);
+  if(isJobChecked) {
+    maxNum +=1;
+  }
+  if(isCommunityChecked) {
+    maxNum +=1;
+  }
+  if(isParentingChecked) {
+    maxNum +=1;
+  }
+  if(isSchoolChecked) {
+    maxNum +=1;
+  }
+  if(isRecoveryChecked) {
+    maxNum +=1;
+  }
+  if(isNoneChecked) {
+    maxNum +=1;
+  }
   const percentageComplete = (stepNum / maxNum) * 100;
 
   if (stepNum === 0) {
@@ -133,7 +186,7 @@ const FormHeader = () => {
       <ProgressBar percentage={percentageComplete} />
 
       {stepNum < maxNum && (
-        <div className={classes.stepText}>Step {stepNum} of 5</div>
+        <div className={classes.stepText}>Step {stepNum} of {maxNum}</div>
       )}
       {stepNum === maxNum && <div className={classes.stepText}>Completed</div>}
     </div>
