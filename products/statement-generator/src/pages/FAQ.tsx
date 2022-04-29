3bcfbf54e67c92b5c085f5a20ab94544b2af9da8
aahvocado <dan.dan.makes.stuff@gmail.com>
2022-04-28 17:58:56 -0700

    update all FAQs to be separate paragraphs instead of two newlines

--- products/statement-generator/src/pages/FAQ.tsx
+++ products/statement-generator/src/pages/FAQ.tsx
@@ -25,31 +25,40 @@ function FAQ() {
 
       <h3>{t('faq_page.title2')}</h3>
       <p>{t('faq_page.paragraph2')}</p>
+      <p>{t('faq_page.paragraph2b')}</p>
+      <p>{t('faq_page.paragraph2c')}</p>
+      <p>{t('faq_page.paragraph2d')}</p>
+      <p>{t('faq_page.paragraph2e')}</p>
 
       <h3>{t('faq_page.title3')}</h3>
       <p>{t('faq_page.paragraph3')}</p>
+      <p>{t('faq_page.paragraph3b')}</p>
 
       <h3>{t('faq_page.title4')}</h3>
       <p>{t('faq_page.paragraph4')}</p>
+      <p>{t('faq_page.paragraph4b')}</p>
+      <p>{t('faq_page.paragraph4c')}</p>
 
       <h3>{t('faq_page.title5')}</h3>
       <p>{t('faq_page.paragraph5')}</p>
-      <ul>
-        <li>{t('faq_page.paragraph5_li1')}</li>
-        <li>{t('faq_page.paragraph5_li2')}</li>
-        <li>{t('faq_page.paragraph5_li3')}</li>
-      </ul>
       <p>{t('faq_page.paragraph5b')}</p>
       <ul>
         <li>{t('faq_page.paragraph5b_li1')}</li>
         <li>{t('faq_page.paragraph5b_li2')}</li>
         <li>{t('faq_page.paragraph5b_li3')}</li>
-        <li>{t('faq_page.paragraph5b_li4')}</li>
-        <li>{t('faq_page.paragraph5b_li5')}</li>
-        <li>{t('faq_page.paragraph5b_li6')}</li>
+      </ul>
+      <p>{t('faq_page.paragraph5c')}</p>
+      <p>{t('faq_page.paragraph5d')}</p>
+      <ul>
+        <li>{t('faq_page.paragraph5d_li1')}</li>
+        <li>{t('faq_page.paragraph5d_li2')}</li>
+        <li>{t('faq_page.paragraph5d_li3')}</li>
+        <li>{t('faq_page.paragraph5d_li4')}</li>
+        <li>{t('faq_page.paragraph5d_li5')}</li>
+        <li>{t('faq_page.paragraph5d_li6')}</li>
       </ul>
       <p>
-        {t('faq_page.paragraph5c')}
+        {t('faq_page.paragraph5e')}
         <LinkAsText link="https://www.courts.ca.gov/documents/Prop47FAQs.pdf" />
       </p>
 
@@ -64,28 +73,34 @@ function FAQ() {
       </ul>
 
       <p>{t('faq_page.paragraph6c')}</p>
+      <p>{t('faq_page.paragraph6d')}</p>
       <ul>
-        <li>{t('faq_page.paragraph6c_li1')}</li>
-        <li>{t('faq_page.paragraph6c_li2')}</li>
-        <li>{t('faq_page.paragraph6c_li3')}</li>
-        <li>{t('faq_page.paragraph6c_li4')}</li>
+        <li>{t('faq_page.paragraph6d_li1')}</li>
+        <li>{t('faq_page.paragraph6d_li2')}</li>
+        <li>{t('faq_page.paragraph6d_li3')}</li>
+        <li>{t('faq_page.paragraph6d_li4')}</li>
       </ul>
+      <p>{t('faq_page.paragraph6e')}</p>
+      <p>{t('faq_page.paragraph6f')}</p>
       <p>
-        {t('faq_page.paragraph6d')}
+        {t('faq_page.paragraph6g')}
         <LinkAsText link="https://drugpolicy.org/sites/default/files/Prop64-Resentencing-Guide-July2017.pdf" />
       </p>
 
       <h3>{t('faq_page.title7')}</h3>
       <p>{t('faq_page.paragraph7')}</p>
+      <p>{t('faq_page.paragraph7b')}</p>
 
       <h3>{t('faq_page.title8')}</h3>
       <p>{t('faq_page.paragraph8')}</p>
 
       <h3>{t('faq_page.title9')}</h3>
       <p>{t('faq_page.paragraph9')}</p>
+      <p>{t('faq_page.paragraph9b')}</p>
 
       <h3>{t('faq_page.title10')}</h3>
       <p>{t('faq_page.paragraph10')}</p>
+      <p>{t('faq_page.paragraph10b')}</p>
 
       <div className={utilityClasses.buttonContainer}>
         <Button
