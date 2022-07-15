9656613c00680c74b8c32054f265f72dba0d9b0d
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-07-15 16:14:19 -0700

    nav bar updates - header stylistic changes are done, but need to handle sticky effect on rest of pages

--- products/statement-generator/src/components-layout/AppHeader.tsx
+++ products/statement-generator/src/components-layout/AppHeader.tsx
@@ -18,8 +18,11 @@ const useStyles = makeStyles<Theme, IUseUtilityStyle>(
         height: 60,
         flex: '0 0 auto',
         background: '#fff',
-        // position: 'fixed',
-        // top: 0,
+        boxShadow: '0px 9px 13px 0px rgb(0,0,0,0.07)',
+        zIndex: 1100,
+        position: 'fixed',
+        top: 0,
+        width: '100%',
 
         [breakpoints.down(breakpoints.values.md)]: {
           background: palette.primary.lighter,
@@ -48,6 +51,8 @@ const useStyles = makeStyles<Theme, IUseUtilityStyle>(
         padding: spacing(1),
         fontSize: '16px',
         fontWeight: 500,
+        display: 'flex',
+        alignItems: 'center',
 
         '&:hover': {
           color: palette.primary.main,
@@ -59,6 +64,10 @@ const useStyles = makeStyles<Theme, IUseUtilityStyle>(
         '&$headerLink + $headerLink': {
           marginLeft: 4,
         },
+      },
+      linkButtonComponent: {
+        padding: '12px 18px !important',
+        marginLeft: '15px !important',
       },
     })
 );
@@ -100,6 +109,7 @@ const AppHeader = () => {
           <LinkButtonComponent
             to={AppUrl.Welcome}
             buttonText={t('button.startNow')}
+            className={classes.linkButtonComponent}
           />
         </div>
       </div>
