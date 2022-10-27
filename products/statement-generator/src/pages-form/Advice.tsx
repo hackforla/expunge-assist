4c5b225a2887aca269d8595a2d6a82d371f1f54b
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-10-26 20:35:23 -0700

    initial structure and copy change for advice page, added icons

--- products/statement-generator/src/pages-form/Advice.tsx
+++ products/statement-generator/src/pages-form/Advice.tsx
@@ -4,16 +4,50 @@ import { useTranslation } from 'react-i18next';
 import ContentContainer from 'components-layout/ContentContainer';
 import FlowNavigation from 'components-layout/FlowNavigation';
 
+import YesIcon from 'assets/Check.png';
+import NoIcon from 'assets/Cancel.png';
+
 export default function Advice() {
   const { t } = useTranslation();
 
   return (
     <ContentContainer>
       <h2>{t('advice_page.header')}</h2>
-      <p>{t('advice_page.point1.content')}</p>
-      
-      <p>{t('advice_page.point2.content')}</p>
-      <p>{t('advice_page.point3.content')}</p>
+      <div>
+        <p>{t('advice_page.point1.content')}</p>
+        <div className="yes-no-container">
+          <div className="no-container">
+            <img src={NoIcon} alt=""/>
+            <p>{t('advice_page.point1.no')}</p>
+          </div>
+          <div className="yes-container">
+            <img src={YesIcon} alt=""/>
+            <p>{t('advice_page.point1.yes')}</p>
+          </div>
+        </div>
+        <p>{t('advice_page.point2.content')}</p>
+        <div className="yes-no-container">
+          <div className="no-container">
+            <img src={NoIcon} alt=""/>
+            <p>{t('advice_page.point2.no')}</p>
+          </div>
+          <div className="yes-container">
+            <img src={YesIcon} alt=""/>
+            <p>{t('advice_page.point2.yes')}</p>
+          </div>
+        </div>
+        <p>{t('advice_page.point3.content')}</p>
+        <div className="yes-no-container">
+          <div className="no-container">
+            <img src={NoIcon} alt=""/>
+            <p>{t('advice_page.point3.no')}</p>
+          </div>
+          <div className="yes-container">
+            <img src={YesIcon} alt=""/>
+            <p>{t('advice_page.point3.yes')}</p>
+          </div>
+        </div>
+      </div>
       <p>{t('advice_page.followUp')}</p>
       <FlowNavigation />
     </ContentContainer>
