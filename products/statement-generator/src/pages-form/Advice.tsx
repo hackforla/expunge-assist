045e10dfcfd4b2ebafc584e991e67eaa8785a3c7
Daniel Xiao <dan.dan.makes.stuff@gmail.com>
2022-11-03 18:23:58 -0700

    lint on Advice.tsx

--- products/statement-generator/src/pages-form/Advice.tsx
+++ products/statement-generator/src/pages-form/Advice.tsx
@@ -76,22 +76,28 @@ export default function Advice() {
         <p className={classes.text}>{t('advice_page.point1.content')}</p>
         <div className={classes.yesNoContainer}>
           <div className={`${classes.borderContainer} ${classes.marginRight}`}>
-            <CancelIcon style={{ color: '#E87461'}} className={classes.icon} />
+            <CancelIcon style={{ color: '#E87461' }} className={classes.icon} />
             <p className={classes.textMargin}>{t('advice_page.point1.no')}</p>
           </div>
           <div className={classes.borderContainer}>
-            <CheckCircleIcon style={{ color: '#0AEBA0'}} className={classes.icon} />
+            <CheckCircleIcon
+              style={{ color: '#0AEBA0' }}
+              className={classes.icon}
+            />
             <p className={classes.textMargin}>{t('advice_page.point1.yes')}</p>
           </div>
         </div>
         <p className={classes.text}>{t('advice_page.point2.content')}</p>
         <div className={classes.yesNoContainer}>
           <div className={`${classes.borderContainer} ${classes.marginRight}`}>
-            <CancelIcon style={{ color: '#E87461'}} className={classes.icon} />
+            <CancelIcon style={{ color: '#E87461' }} className={classes.icon} />
             <p className={classes.textMargin}>{t('advice_page.point2.no')}</p>
           </div>
           <div className={classes.borderContainer}>
-            <CheckCircleIcon style={{ color:'#0AEBA0'}} className={classes.icon} />
+            <CheckCircleIcon
+              style={{ color: '#0AEBA0' }}
+              className={classes.icon}
+            />
             <p className={classes.textMargin}>{t('advice_page.point2.yes')}</p>
           </div>
         </div>
@@ -102,7 +108,10 @@ export default function Advice() {
             <p className={classes.textMargin}>{t('advice_page.point3.no')}</p>
           </div>
           <div className={classes.borderContainer}>
-            <CheckCircleIcon style={{ color: '#0AEBA0'}} className={classes.icon} />
+            <CheckCircleIcon
+              style={{ color: '#0AEBA0' }}
+              className={classes.icon}
+            />
             <p className={classes.textMargin}>{t('advice_page.point3.yes')}</p>
           </div>
         </div>
