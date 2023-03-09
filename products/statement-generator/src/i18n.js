d2ca10009d1fc97392a9a95f180040997ddb9a0f
Tom Glaz <tgmessages@gmail.com>
2023-03-08 22:17:51 -0800

    update i18n backend.loadPath to work with forked github pages

--- products/statement-generator/src/i18n.js
+++ products/statement-generator/src/i18n.js
@@ -3,6 +3,24 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
+function isGithubDomain() {
+  const { host } = window.location;
+
+  return host.includes('github.io');
+}
+
+function getLoadPath() {
+  const { pathname } = window.location;
+
+  let urlPath = '/';
+
+  if (isGithubDomain()) {
+    urlPath = pathname;
+  }
+
+  return `${urlPath}locales/{{lng}}/{{ns}}.json`;
+}
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
@@ -16,7 +34,7 @@ i18n
       cache: ['cookie'],
     },
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      loadPath: getLoadPath(),
     },
   });
 
