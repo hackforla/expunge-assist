0a7694bc7301cf9e41b1bd43b99ac71bd97db0a7
Sydney Walcoff <sydney.walcoff@gmail.com>
2023-05-25 18:15:01 -0700

    linted

--- products/statement-generator/src/pages/AboutUs.tsx
+++ products/statement-generator/src/pages/AboutUs.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles, createStyles, Link } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 
-import ButtonComponent, { LinkButtonComponent } from 'components/Button';
+import ButtonComponent from 'components/Button';
 
 import ContentContainer from 'components-layout/ContentContainer';
 
@@ -41,101 +41,95 @@ export default function AboutUs() {
   };
   return (
     <ContentContainer>
-        <h2>{t('about_us_page.page_header')}</h2>
-        <h3>{t('about_us_page.title1')}</h3>
-        <br />
-        <p className={classes.paragraphStyle}>
-          {t('about_us_page.paragraph1')}
-        </p>
-        <br />
+      <h2>{t('about_us_page.page_header')}</h2>
+      <h3>{t('about_us_page.title1')}</h3>
+      <br />
+      <p className={classes.paragraphStyle}>{t('about_us_page.paragraph1')}</p>
+      <br />
 
-        <ButtonComponent
-          onClick={() => handleMeetTheTeam()}
-          buttonText={t('Meet the Team')}
-          className={classes.buttonStyle}
-        />
+      <ButtonComponent
+        onClick={() => handleMeetTheTeam()}
+        buttonText={t('Meet the Team')}
+        className={classes.buttonStyle}
+      />
 
-        <br />
-        <h3>{t('about_us_page.title2')}</h3>
-        <br />
-        <ul className={classes.paragraphStyle}>
-          <li className={classes.pointStyle}>
-            <span className={classes.spanStyle}>
-              Expunge Assist is part of{' '}
-              <Link
-                underline="always"
-                className={classes.linkStyle}
-                href="https://www.hackforla.org/"
-              >
-                Hack for LA
-              </Link>{' '}
-              and{' '}
-              <Link
-                underline="always"
-                className={classes.linkStyle}
-                href="https://codeforamerica.org/"
-              >
-                Code for America{' '}
-              </Link>
-            </span>
-          </li>
-          <li className={classes.pointStyle}>
-            <span className={classes.spanStyle}>
-              {t('about_us_page.paragraph2_li2')}
-            </span>
-          </li>
-          <li className={classes.pointStyle}>
-            <span className={classes.spanStyle}>
-              {t('about_us_page.paragraph2_li3')}
-            </span>
-          </li>
-        </ul>
-        <p className={classes.paragraphStyle}>
-          <Link
-            underline="always"
-            className={classes.linkStyle}
-            href="https://codeforamerica.org/programs/criminal-justice/automatic-record-clearance/"
-          >
-            *source
-          </Link>
-        </p>
-        <h3>{t('about_us_page.title3')}</h3>
-        <br />
-        <ul className={classes.paragraphStyle}>
-          <li className={classes.pointStyle}>
-            <span className={classes.spanStyle}>
-              {t('about_us_page.paragraph3_li1')}
-            </span>
-          </li>
-          <li className={classes.pointStyle}>
-            <span className={classes.spanStyle}>
-              {t('about_us_page.paragraph3_li2')}
-            </span>
-          </li>
-          <li className={classes.pointStyle}>
-            <span className={classes.spanStyle}>
-              {t('about_us_page.paragraph3_li3')}
-            </span>
-          </li>
-        </ul>
-        <h3>{t('about_us_page.title4')}</h3>
-        <br />
-        <p className={classes.paragraphStyle}>
-          {t('about_us_page.paragraph4')}
-        </p>
-        <br />
-        <p className={classes.paragraphStyle}>
-          {t('about_us_page.paragraph4b')}
-        </p>
-        <br />
+      <br />
+      <h3>{t('about_us_page.title2')}</h3>
+      <br />
+      <ul className={classes.paragraphStyle}>
+        <li className={classes.pointStyle}>
+          <span className={classes.spanStyle}>
+            Expunge Assist is part of{' '}
+            <Link
+              underline="always"
+              className={classes.linkStyle}
+              href="https://www.hackforla.org/"
+            >
+              Hack for LA
+            </Link>{' '}
+            and{' '}
+            <Link
+              underline="always"
+              className={classes.linkStyle}
+              href="https://codeforamerica.org/"
+            >
+              Code for America{' '}
+            </Link>
+          </span>
+        </li>
+        <li className={classes.pointStyle}>
+          <span className={classes.spanStyle}>
+            {t('about_us_page.paragraph2_li2')}
+          </span>
+        </li>
+        <li className={classes.pointStyle}>
+          <span className={classes.spanStyle}>
+            {t('about_us_page.paragraph2_li3')}
+          </span>
+        </li>
+      </ul>
+      <p className={classes.paragraphStyle}>
+        <Link
+          underline="always"
+          className={classes.linkStyle}
+          href="https://codeforamerica.org/programs/criminal-justice/automatic-record-clearance/"
+        >
+          *source
+        </Link>
+      </p>
+      <h3>{t('about_us_page.title3')}</h3>
+      <br />
+      <ul className={classes.paragraphStyle}>
+        <li className={classes.pointStyle}>
+          <span className={classes.spanStyle}>
+            {t('about_us_page.paragraph3_li1')}
+          </span>
+        </li>
+        <li className={classes.pointStyle}>
+          <span className={classes.spanStyle}>
+            {t('about_us_page.paragraph3_li2')}
+          </span>
+        </li>
+        <li className={classes.pointStyle}>
+          <span className={classes.spanStyle}>
+            {t('about_us_page.paragraph3_li3')}
+          </span>
+        </li>
+      </ul>
+      <h3>{t('about_us_page.title4')}</h3>
+      <br />
+      <p className={classes.paragraphStyle}>{t('about_us_page.paragraph4')}</p>
+      <br />
+      <p className={classes.paragraphStyle}>{t('about_us_page.paragraph4b')}</p>
+      <br />
 
-        <ButtonComponent
-          onClick={() => handleContactUs()}
-          buttonText={t('Contact Us')}
-          className={classes.buttonStyle}
-        />
+      <ButtonComponent
+        onClick={() => handleContactUs()}
+        buttonText={t('Contact Us')}
+        className={classes.buttonStyle}
+      />
 
-        <br />
+      <br />
     </ContentContainer>
   );
 }
