65aa85782d60cd271629a28c11a1aa0bd81126b3
David Wiese <21321101+davidwiese@users.noreply.github.com>
2023-11-29 11:43:29 -0800

    Install Lottie

--- products/statement-generator/src/components-layout/LoadingPage.tsx
+++ products/statement-generator/src/components-layout/LoadingPage.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useLottie } from 'lottie-react';
+import loadingData from '../assets/loading.json';
 
 const LoadingPage: React.FC = () => {
-  return <p>TESTING</p>;
+  const options = {
+    animationData: loadingData,
+    loop: true,
+  };
+
+  const { View } = useLottie(options);
+
+  return (
+    <div>
+      <p>TESTING</p>
+      {View}
+    </div>
+  );
 };
 
 export default LoadingPage;
