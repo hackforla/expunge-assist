9072310d988832a5f1fd4a254fda5a4ec6c642bb
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-01-11 15:03:18 -0800

    Added explicit TypeScript types to component

--- products/statement-generator/src/components-layout/Loading.tsx
+++ products/statement-generator/src/components-layout/Loading.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { CircularProgress } from '@material-ui/core';
 import styles from '../styles/Loading.module.css';
 
-export default function Loading() {
+interface LoadingProps {}
+
+const Loading: React.FC<LoadingProps> = () => {
   return (
     <div className={styles.wrapper}>
       <CircularProgress size="2rem" style={{ color: '#9903ff' }} />
       <h1>Loading</h1>
     </div>
   );
-}
+};
+
+export default Loading;
