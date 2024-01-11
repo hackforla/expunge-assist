76c6eb47763fbfd5fd640fe54daf454041be96e5
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-01-11 10:40:19 -0800

    Implement suspense fallback loading component

--- products/statement-generator/src/index.tsx
+++ products/statement-generator/src/index.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 
 import 'styles/cssreset.css';
+import styles from 'styles/index.module.css';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -11,13 +12,21 @@ import './i18n';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loading />}>
       <App />
     </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+function Loading() {
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.loader} />
+      <h1>Loading</h1>
+    </div>
+  );
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
