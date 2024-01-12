d49d6190132fb3130943b9fd98ed0064dd5944b7
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-01-12 09:29:24 -0800

    Style loading component with MUI

--- products/statement-generator/src/components-layout/Loading.tsx
+++ products/statement-generator/src/components-layout/Loading.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { CircularProgress } from '@material-ui/core';
-import styles from '../styles/Loading.module.css';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    wrapper: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      gap: '1.5em',
+    },
+    circularProgress: {
+      color: '#9903FF',
+    },
+  })
+);
 
 interface LoadingProps {}
 
 const Loading: React.FC<LoadingProps> = () => {
+  const classes = useStyles();
+
   return (
-    <div className={styles.wrapper}>
-      <CircularProgress size="2rem" style={{ color: '#9903ff' }} />
+    <div className={classes.wrapper}>
+      <CircularProgress className={classes.circularProgress} size="2rem" />
       <h1>Loading</h1>
     </div>
   );
