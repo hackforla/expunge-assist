b9b5917dac6ddce7f0a24d3866b88615d64a605e
Raman Kala <rs.kala@hotmail.com>
2024-01-15 17:34:54 -0500

    refactor: change styling of light purple background to match wireframes

--- products/statement-generator/src/components/FormHeader.tsx
+++ products/statement-generator/src/components/FormHeader.tsx
@@ -11,15 +11,17 @@ import { IInvolvementInitialState } from 'contexts/FormStateProps';
 import { getSectionTitle } from 'helpers/i18nHelper';
 
 const useStyles = makeStyles<Theme>(
-  ({ palette, breakpoints, globals, spacing, typography }) =>
+  ({ palette, globals, spacing, typography }) =>
     createStyles({
       outerWrapper: {
         background: palette.primary.lighter,
         borderBottomRightRadius: '64px',
         paddingBottom: spacing(1),
-
-        [breakpoints.up(breakpoints.values.md)]: {
-          borderBottomLeftRadius: '64px',
+        maxWidth: '80%',
+        marginLeft: '10%',
+        '@media (max-height:759px)': {
+          maxWidth: '100%',
+          marginLeft: '0%',
         },
       },
       formHeader: {
