2c318047878bd8d379773f9e0f3287ad09d0e376
Sydney Walcoff <sydney.walcoff@gmail.com>
2024-03-27 14:33:51 -0700

    run prettier

--- products/statement-generator/src/tests/placeholder.test.tsx
+++ products/statement-generator/src/tests/placeholder.test.tsx
@@ -2,11 +2,10 @@ import React from 'react'; // necessary to render components
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-
 it('Tests execute', () => {
-    const mockFn = jest.fn();
-    mockFn.mockReturnValue(true);
+  const mockFn = jest.fn();
+  mockFn.mockReturnValue(true);
 
-    expect(mockFn()).toBe(true);
-    expect(mockFn()).not.toBe(false);
-})
+  expect(mockFn()).toBe(true);
+  expect(mockFn()).not.toBe(false);
+});
