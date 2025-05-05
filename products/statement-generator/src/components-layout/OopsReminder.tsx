37b6f9747fbf90eaf74282d60f21d9a20503cd66
Sydney Walcoff <sydney.walcoff@gmail.com>
2025-05-05 16:36:40 -0700

    building oops reminder

--- products/statement-generator/src/components-layout/OopsReminder.tsx
+++ products/statement-generator/src/components-layout/OopsReminder.tsx
@@ -1,26 +1,63 @@
 import React, { useContext } from 'react';
+import { Theme, createStyles, makeStyles } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 
 import FormStateContext from 'contexts/FormStateContext';
 import { AppUrl } from 'contexts/RoutingProps';
 
 import { LinkButtonComponent } from 'components/Button';
+
 import OopsReminderImage from 'assets/oopsReminder.svg';
+import useUtilityStyles from 'styles/utilityStyles';
+
+const useStyles = makeStyles<Theme>(({ spacing, globals }) => createStyles({
+  outerOopsWrap: {
+    padding: spacing(2),
+    display: 'flex',
+  },
+  oopsHeader: {
+    fontSize: '60px',
+    lineHeight: '90px',
+  },
+  oopsText: {
+    fontSize: '24px',
+    lineHeight: '36px',
+  },
+
+  buttonContainer: {
+    width: '100%',
+    display: 'flex',
+    padding: spacing(3, 0),
+  },
+  contentContainer: {
+    width: '50%',
+  },
+  textContainer: {
+    minWidth: '540px',
+
+  }
+}));
 
 const OopsReminder = () => {
   const { t } = useTranslation();
   const { showOopsReminder } = useContext(FormStateContext);
+  const utilityClasses = useUtilityStyles();
+  const styles = useStyles();
+
   if (!showOopsReminder) return null;
   return (
-    <div className="outer-oops-wrap">
-      <div className="text-container">
-        <h2>{t('oops_reminder.header')}</h2>
-        <p>{t('oops_reminder.paragraph_1')}</p>
-        <p>{t('oops_reminder.paragraph_2')}</p>
-        <LinkButtonComponent buttonText={t('oops_reminder.button_text')} to={AppUrl.Introduction}/>
+    <div className={`${utilityClasses.widePage} ${styles.outerOopsWrap}`}>
+      <div className={`text-container ${styles.textContainer}`}>
+        <h1 className={styles.oopsHeader}>{t('oops_reminder.header')}</h1>
+        <p className={styles.oopsText}>{t('oops_reminder.paragraph_1')}</p>
+        <br />
+        <p className={styles.oopsText}>{t('oops_reminder.paragraph_2')}</p>
+        <div className={styles.buttonContainer}>
+          <LinkButtonComponent buttonText={t('oops_reminder.button_text')} to={AppUrl.Introduction} />
+        </div>
       </div>
-      <div className="image-container">
-        <img src={OopsReminderImage} alt="Someone with their hands on their head in frustration standing in front of a computer that says 'NO REFRESHING'" />
+      <div className={`image-container ${styles.contentContainer}`}>
+        <img src={OopsReminderImage} alt={t('oops_reminder.image_alt_text')} />
       </div>
     </div>
   );
