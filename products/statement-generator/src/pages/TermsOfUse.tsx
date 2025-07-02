64e8e7c2fe669e93d1d0bc3032963977f49989e7
Javier Barrera <javierbarrera167@gmail.com>
2025-07-01 22:23:50 -0700

    added new line height to terms of Use page

--- products/statement-generator/src/pages/TermsOfUse.tsx
+++ products/statement-generator/src/pages/TermsOfUse.tsx
@@ -2,22 +2,38 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import ContentContainer from 'components-layout/ContentContainer';
+import { createStyles, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    text: {
+      lineHeight: 1.5,
+    },
+  })
+);
 
 function TermsOfUse() {
   const { t } = useTranslation();
+  const classes = useStyles();
 
   return (
     <ContentContainer>
       <h2>{t('links.terms_of_use')}</h2>
       <h3>{t('terms_of_use_page.generator_heading')}</h3>
-      <p>{t('terms_of_use_page.generator_description')}</p>
+      <p className={classes.text}>
+        {t('terms_of_use_page.generator_description')}
+      </p>
 
       <h3>{t('terms_of_use_page.scope_heading')}</h3>
-      <p>{t('terms_of_use_page.scope_description')}</p>
+      <p className={classes.text}>{t('terms_of_use_page.scope_description')}</p>
 
       <h3>{t('terms_of_use_page.contributions_heading')}</h3>
-      <p>{t('terms_of_use_page.contributions_description')}</p>
-      <p>{t('terms_of_use_page.liability_description')}</p>
+      <p className={classes.text}>
+        {t('terms_of_use_page.contributions_description')}
+      </p>
+      <p className={classes.text}>
+        {t('terms_of_use_page.liability_description')}
+      </p>
     </ContentContainer>
   );
 }
